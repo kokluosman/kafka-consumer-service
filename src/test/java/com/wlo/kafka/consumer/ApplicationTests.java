5ca87e70c99cd0df2a05f4351d004631c6d6d0d8
wloso <kokluosman01@gmail.com>
2024-07-29 22:22:20 +0300

    Initial commit

---
+++ src/test/java/com/wlo/kafka/consumer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wlo.kafka.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
